590782cf07586a2aaa0995a1791ae5d43b50a224
Alex Weis <alex.weis25@gmail.com>
2018-04-28 11:37:07 -0500

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,26 @@
+const functions = require('firebase-functions');
+let request = require("request");
+let querystring = require("querystring");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+
+exports.login = functions.https.onRequest((req, res) => {
+
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        scope:
+          "user-read-private user-read-email playlist-modify-public playlist-read-collaborative playlist-read-private playlist-modify-private user-read-currently-playing",
+        redirect_uri
+      })
+  );
+
+})
